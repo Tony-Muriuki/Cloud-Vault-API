adc24c7a3227a0a2da561db83db5121cb165b2c8
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-24 23:38:08 +0300

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//Root Module ->Use all submodules
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
