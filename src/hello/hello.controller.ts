296d46e5500df3c9f76607730105ed6be6f73a5a
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-11-25 14:28:50 +0300

    Module

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
 
+//Express
+//server.js -> routes-> controller -> services
+//Route A -Controller A -> Service A
+//Incoming Requests and Responses
+//How controllers work is that they use DI(Dependency Injection) to consume services to handle requests and responses but delegate businesss logic to services.
 @Controller('hello')
 export class HelloController {}
